cbfc00fca5b619a45a96bae2279e94a2868da192
Peter Albert <peter.albert@data2impact.com>
2020-10-02 01:09:21 +0200

    Add manual create SQL

--- script.ts
+++ script.ts
@@ -1,7 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, sql } from "@prisma/client";
 
-const prisma = new PrismaClient();
-// { log: ["query"] }
+const prisma = new PrismaClient({});
 
 const emptyDatabase = async () => {
   await prisma.post.deleteMany({});
@@ -24,13 +23,36 @@ const createUserWithPosts = async (postCount: number) => {
         })),
       },
     },
-    // include: { posts: true },
   });
 };
+
+const createUserWithPosts2 = async (postCount: number) => {
+  await prisma.$executeRaw`INSERT INTO User (email, name) VALUES (${"alice@prisma.io"}, ${"Alice"})`;
+  const userId = (
+    await prisma.user.findOne({
+      where: { email: "alice@prisma.io" },
+    })
+  )?.id;
+  await prisma.$executeRaw(
+    `INSERT INTO Post (title, content, published, authorId) VALUES ` +
+      range(postCount)
+        .map((n) => `("1", "1", true, ${userId})`)
+        .join(",")
+  );
+};
 async function main() {
+  const postCount = 100000;
+  await emptyDatabase();
+
   console.time("create user");
-  const user = await createUserWithPosts(5000);
+  await createUserWithPosts(postCount);
   console.timeEnd("create user");
+
+  await emptyDatabase();
+
+  console.time("create user - manual");
+  await createUserWithPosts2(postCount);
+  console.timeEnd("create user - manual");
   await emptyDatabase();
 }
 
